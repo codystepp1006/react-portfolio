031829c7e93bbecf9eb6e13694823137113f4e30
Cody Stepp <codystepp691@gmail.com>
2024-03-13 17:29:12 -0500

    added redirects file trying to deploy

--- Portfolio/server/server.js
+++ Portfolio/server/server.js
@@ -3,28 +3,9 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 
-const { authMiddleware } = require('');
-
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
-
-const startApolloServer = async () => {
-  await server.start();
-  
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-  
-  app.use('/graphql', expressMiddleware(server, { 
-    context: authMiddleware
-  }));
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
@@ -35,12 +16,4 @@ const startApolloServer = async () => {
     });
   } 
 
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-    });
-  });
-};
 
-startApolloServer();
